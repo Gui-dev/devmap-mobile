596e64c9117f540d08fdf40650682b246cc8d81b
Gui-dev <adrianon10@gmail.com>
2020-02-09 16:14:10 -0300

    first commit

---
+++ src/routes/index.js
@@ -0,0 +1,12 @@
+import { NavigationContainer } from '@react-navigation/native'
+
+function Routes() {
+
+  return (
+    <NavigationContainer>
+
+    </NavigationContainer>
+  )
+}
+
+export default Routes
